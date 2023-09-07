1649c434b64b5ffbbefb2a1b4fb5bea0999cd2cd
micaels94 <96264952+micaels94@users.noreply.github.com>
2023-09-07 02:39:54 +0100

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,10 @@
+const ageIncrementing = () => {
+    // variables
+    //var ageId = document.getElementById("age-script"); 
+    var dateStartedCoding = new Date("April 17, 2020") //day of the week / month / day / year
+    console.log(dateStartedCoding);
+}
+
+
+var dateStartedCoding = new Date("April 17, 2020") //day of the week / month / day / year
+console.log(dateStartedCoding)
